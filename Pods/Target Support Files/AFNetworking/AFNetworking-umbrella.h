a65ec389b919e2651b9070d30ce648e532e242ec
yharshal <yanpallewarharshal@gmail.com>
2019-07-05 01:09:08 +0530

    Changed pods

---
+++ Pods/Target Support Files/AFNetworking/AFNetworking-umbrella.h
@@ -0,0 +1,25 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AFURLConnectionOperation.h"
+#import "AFHTTPRequestOperation.h"
+#import "AFHTTPRequestOperationManager.h"
+#import "AFURLSessionManager.h"
+#import "AFHTTPSessionManager.h"
+#import "AFNetworkReachabilityManager.h"
+#import "AFSecurityPolicy.h"
+#import "AFURLRequestSerialization.h"
+#import "AFURLResponseSerialization.h"
+
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
+
